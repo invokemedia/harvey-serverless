f2ec4882d20f6eb6b422097d633e7973a109b6fd
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-04-07 12:33:01 -0600

    action test

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,13 +2,8 @@ import { HarvestClient } from "./harvest.client";
 import { SlackClient } from "./slack.client";
 import { HarveyHandler } from "./harvey.handler";
 
-const harvest = new HarvestClient(
-  "TOKEN",
-  "ID"
-);
-const slack = new SlackClient(
-  "HOOK"
-);
+const harvest = new HarvestClient(process.env.HARVEST_AUTHORIZATION_TOKEN, process.env.HARVEST_ACCOUNT_ID);
+const slack = new SlackClient(process.env.SLACK_WEBHOOK_URL);
 const handler = new HarveyHandler(harvest, slack);
 
 export const handle = async () => {
